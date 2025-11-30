9a8eb4b78c0c223f9035a264230cdcd7f6088690
Ziyad-000 <Ziyad.sayed1515@compit.aun.edu.eg>
2025-11-29 19:03:49 -0600

    Refactor app routing: add authentication, profile, and support screens; update route constants

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,9 +1,20 @@
-import 'package:ecommerce_app/features/products/models/product_model.dart';
-import 'package:ecommerce_app/main_layout.dart';
-import 'package:ecommerce_app/features/products/views/pages/product_details_screen.dart';
-import 'package:ecommerce_app/features/products/views/pages/product_screen.dart';
 import 'package:flutter/material.dart';
-import '../../features/auth/presentation/pages/splach_screen.dart';
+import '../../features/auth/views/pages/splach_screen.dart';
+import '../../features/auth/views/pages/login_screen.dart';
+import '../../features/auth/views/pages/register_screen.dart';
+import '../../features/auth/views/pages/reset_password_screen.dart';
+import '../../features/cart/views/pages/cart_screen.dart';
+import '../../features/products/models/product_model.dart';
+import '../../features/products/views/pages/product_details_screen.dart';
+import '../../features/products/views/pages/product_screen.dart';
+import '../../features/profile/views/pages/profile_screen.dart';
+import '../../features/profile/views/pages/settings_screen.dart';
+import '../../features/profile/views/pages/help_support_screen.dart';
+import '../../features/profile/views/pages/order_history_screen.dart';
+import '../../features/profile/views/pages/saved_items_screen.dart';
+import '../../features/profile/views/pages/addresses_screen.dart';
+import '../../features/profile/views/pages/payment_methods_screen.dart';
+import '../../main_layout.dart';
 import 'app_routes.dart';
 
 class AppRouter {
@@ -12,8 +23,17 @@ class AppRouter {
       case AppRoutes.splashRoute:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
 
-      case AppRoutes.MainLayoutRoute:
-        return MaterialPageRoute(builder: (_) => MainLayout());
+      case AppRoutes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      case AppRoutes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+
+      case AppRoutes.resetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ResetPasswordScreen());
+
+      case AppRoutes.mainLayoutRoute:
+        return MaterialPageRoute(builder: (_) => const MainLayout());
 
       case AppRoutes.productsRoute:
         return MaterialPageRoute(
@@ -26,38 +46,34 @@ class AppRouter {
       case AppRoutes.productDetailsRoute:
         return MaterialPageRoute(
           builder: (context) {
-            final ProductModel productName = settings.arguments as ProductModel;
-            return ProductDetailsScreen(product: productName);
+            final ProductModel product = settings.arguments as ProductModel;
+            return ProductDetailsScreen(product: product);
           },
         );
 
-      // case AppRoutes.loginRoute:
+      case AppRoutes.profileRoute:
+        return MaterialPageRoute(builder: (_) => const ProfileScreen());
 
-      // case AppRoutes.productDetailsRoute:
-      //   final String productId = settings.arguments as String;
-      //   return MaterialPageRoute(builder: (_) => ProductDetailsScreen(productId: productId));
+      case AppRoutes.cartRoute:
+        return MaterialPageRoute(builder: (_) => const CartScreen());
 
-      // case AppRoutes.loginRoute:
-      // // return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case AppRoutes.orderHistoryRoute:
+        return MaterialPageRoute(builder: (_) => const OrderHistoryScreen());
 
-      //
-      // case AppRoutes.registerRoute:
-      // // return MaterialPageRoute(builder: (_) => const RegisterScreen());
+      case AppRoutes.savedItemsRoute:
+        return MaterialPageRoute(builder: (_) => const SavedItemsScreen());
 
-      //
-      // case AppRoutes.profileRoute:
-      // // return MaterialPageRoute(builder: (_) => const ProfileScreen());
+      case AppRoutes.addressesRoute:
+        return MaterialPageRoute(builder: (_) => const AddressesScreen());
 
-      //
-      // case AppRoutes.cartRoute:
-      // // return MaterialPageRoute(builder: (_) => const CartScreen());
+      case AppRoutes.paymentMethodsRoute:
+        return MaterialPageRoute(builder: (_) => const PaymentMethodsScreen());
 
-      //
+      case AppRoutes.settingsRoute:
+        return MaterialPageRoute(builder: (_) => const SettingsScreen());
 
-      //
-      // case AppRoutes.orderDetailsRoute:
-      //   final String orderId = settings.arguments as String;
-      //   // return MaterialPageRoute(builder: (_) => OrderDetailsScreen(orderId: orderId));
+      case AppRoutes.helpSupportRoute:
+        return MaterialPageRoute(builder: (_) => const HelpSupportScreen());
 
       default:
         return MaterialPageRoute(
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,14 +1,21 @@
 class AppRoutes {
-  static const String splashRoute = '/splash';
+  static const String splashRoute = '/';
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String resetPasswordRoute = '/reset-password';
+  static const String mainLayoutRoute = '/main-layout';
   static const String homeRoute = '/home';
   static const String categoriesRoute = '/categories';
   static const String productsRoute = '/products';
   static const String searchRoute = '/search';
-  static const String MainLayoutRoute = '/main-layout';
-  static const String loginRoute = '/login';
-  static const String registerRoute = '/register';
   static const String profileRoute = '/profile';
   static const String cartRoute = '/cart';
-  static const String productDetailsRoute = '/productDetails'; // تحتاج لـ ID
-  static const String orderDetailsRoute = '/order-details'; // تحتاج لـ ID
+  static const String productDetailsRoute = '/productDetails';
+  static const String orderDetailsRoute = '/order-details';
+  static const String orderHistoryRoute = '/order-history';
+  static const String savedItemsRoute = '/saved-items';
+  static const String addressesRoute = '/addresses';
+  static const String paymentMethodsRoute = '/payment-methods';
+  static const String settingsRoute = '/settings';
+  static const String helpSupportRoute = '/help-support';
 }
